ade0f539e2b48e3e0b9627171d4405868c77add7
Daniel Baron <danbaron63@gmail.com>
2025-02-09 19:18:28 +0000

    added model tests

---
+++ tests/test_model.py
@@ -0,0 +1,19 @@
+from polar_streams.model import MicroBatch, Metadata
+from datetime import datetime
+import polars as pl
+
+
+def test_microbatch_new():
+    # Given
+    df = pl.DataFrame({"col1": [1, 2, 3]}).lazy()
+    start_time = datetime.now()
+    metadata = Metadata(start_time=start_time)
+    microbatch = MicroBatch(pl_df=df, metadata=metadata)
+
+    # When
+    new_microbatch = microbatch.new(df)
+
+    # Then
+    assert id(microbatch) != id(new_microbatch)
+    assert id(microbatch.metadata) == id(new_microbatch.metadata)
+    assert id(microbatch.pl_df) == id(new_microbatch.pl_df)
